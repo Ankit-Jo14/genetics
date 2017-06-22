5fcef0039aec64f0f904a0005ad002e818c25523
Ankit Joshi <mrityunjay1492@gmail.com>
2017-06-22 18:16:27 -0400

    checkbox limit

--- home.js
+++ home.js
@@ -15,14 +15,14 @@
      }
 
      $("#search").click(function() {
-          var $name , $PInumber , $year, $country, $state , $maturityGrp ;
-          $name = $PInumber= $("#cultivar").val().trim().toLowerCase();
-          $year = $("#year").val().trim();
-          $country = $state = $("#origin").val().trim();
-          $maturityGrp = $("#maturityGrp").val().trim();
-         
-         
-          
+         var $name, $PInumber, $year, $country, $state, $maturityGrp;
+         $name = $PInumber = $("#cultivar").val().trim().toLowerCase();
+         $year = $("#year").val().trim();
+         $country = $state = $("#origin").val().trim();
+         $maturityGrp = $("#maturityGrp").val().trim();
+
+
+
          // var searchPart1 = $name ? (row['Cultivar'] == $name || row['PI number'] == $PInumber) : true ;
          // var searchPart2 = $country ? (row['Country'] == $country || row['State'] == $state) : true ;
          // var searchPart3 =  $year ? (row['Year'] == $year) : true;
@@ -31,11 +31,11 @@
          //                                            ($country ? (row['Country'] == $country || row['State'] == $state) : true) && 
          //                                            ($year ? (row['Year'] == $year) : true) && 
          //                                            ($maturityGrp ? (row['MG'] == $maturityGrp) : true));    
-         
-         var filteredData = tableData.filter(row => ($name ? (row['Cultivar'].toLowerCase().includes($name) || row['PI number'].toLowerCase().includes($PInumber)) : true) && 
-                                                    ($country ? (row['Country'] == $country || row['State'] == $state) : true) && 
-                                                    ($year ? (row['Year'] == $year) : true) && 
-                                                    ($maturityGrp ? (row['MG'] == $maturityGrp) : true));  
+
+         var filteredData = tableData.filter(row => ($name ? (row['Cultivar'].toLowerCase().includes($name) || row['PI number'].toLowerCase().includes($PInumber)) : true) &&
+             ($country ? (row['Country'] == $country || row['State'] == $state) : true) &&
+             ($year ? (row['Year'] == $year) : true) &&
+             ($maturityGrp ? (row['MG'] == $maturityGrp) : true));
          generateTable(filteredData);
 
      });
@@ -48,11 +48,11 @@
          d3.csv("data.csv", function(data) {
 
              loader.stop();
-        tableData = data; 
+             tableData = data;
              // setTimeout(function (){loader.stop();} , 10000);
 
              generateTable(data);
-
+             generateAttributeBoxData(tableData);
 
          });
      }
@@ -61,12 +61,12 @@
      function generateTable(data) {
          var $tableHeader = $("#tableHeader");
          var $tableBody = $("#tableBody");
-         var columns = tableData.columns;// get column from main table data as the same fuction is used for filterd data also
+         var columns = tableData.columns; // get column from main table data as the same fuction is used for filterd data also
          var dataCount = data.length;
          var columnsCount = columns.length;
          var elementstoAppend = [];
          var showTreeButtonIdentifier = "Cultivar";
-         
+
          // clear previously displayed table data
          $tableHeader.empty();
          $tableBody.empty();
@@ -83,9 +83,9 @@
          elementstoAppend[elementstoAppendLastIndex] = "<th></th></tr>";
          $tableHeader.html(elementstoAppend.join(''));
 
-        // adding data rows
+         // adding data rows
          elementstoAppend = [];
-        for (var i = 0; i < dataCount; i++) {
+         for (var i = 0; i < dataCount; i++) {
              var dataRow = data[i];
              var rowArray = [];
              for (var j = 0; j < columnsCount; j++) {
@@ -102,9 +102,38 @@
          $tableBody.html(elementstoAppend.join(''));
      }
 
-     function generateAttributeBoxData(tableData){
-        var attributeList = tableData.columns;
-        
+ function generateAttributeBoxData(tableData) {
+         $list = $("#attributeBox > .container");
+         var attributeList = tableData.columns;
+         var columnArray = [];
+         for (var i = 0; i < attributeList.length; i++) {
+             var dataRow = attributeList[i];
+             columnArray[i] = "<div class='checkbox'><label><input type='checkbox' value=" + dataRow + "/>" + dataRow + "</label></div> ";
+
+         }
+
+         $list.html(columnArray.join(''));
 
      }
+
+
+   $(document).on('change', "#attributes input[type='checkbox']", function(e) {
+     var limit = 3;
+
+     if ($("#attributes  input[type='checkbox']:checked").length >= limit) {
+         this.checked = false;
+     }
+
+     console.log("change fired");
+     if (this.checked) {
+         console.log("checked");
+         $(this).attr("value");
+     } else {
+         console.log("unchecked")
+         $(this).attr("value");
+     }
+    })
+     
+
+
  });
